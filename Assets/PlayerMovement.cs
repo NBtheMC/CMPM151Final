87ec6861df97b48ca82f43fde2902f90199106d6
NBtheMC <nvbhusha@ucsc.edu>
2021-02-21 16:44:51 -0800

    Basic player movement
    Very simple. Will add shooting and flipping later

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+    }
+
+    // FixedUpdate used for physics and rigidbodies and stuff
+    void FixedUpdate()
+    {
+        //Actual movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
