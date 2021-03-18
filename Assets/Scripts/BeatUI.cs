f6ee84e303deb7bb2c7dfc4429c40d6f5e93fda5
Nathan Wang <60531792+NathanWang00@users.noreply.github.com>
2021-03-17 19:34:45 -0700

    Update BeatUI.cs

--- Assets/Scripts/BeatUI.cs
+++ Assets/Scripts/BeatUI.cs
@@ -7,13 +7,11 @@ public class BeatUI : MonoBehaviour
     public GameObject ShootingTiming;
     private GameObject finishline, startLine;
     private float tempo, distance;
-    private int beat;
     // Start is called before the first frame update
     void Start()
     {
         ShootingTiming = GameManager.Instance.GetShootingTiming();
         tempo = GameManager.Instance.getTempo();
-        beat = GameManager.Instance.GetShootingTiming().GetComponent<ShootingTiming>().getBeat();
         finishline = GameObject.Find("lineStop");
         startLine = GameObject.Find("lineStart");
         transform.position = startLine.transform.position;
@@ -25,14 +23,13 @@ public class BeatUI : MonoBehaviour
     {
         // get the tempo timing per millisecond
         // get transform of LineStart
-        beat = GameManager.Instance.GetShootingTiming().GetComponent<ShootingTiming>().getBeat();
+        tempo = GameManager.Instance.getTempo();
         transform.Translate(distance / (tempo * 4) * Time.deltaTime, 0, 0);
     }
     void FixedUpdate() {
         // get beat
         // if = 0, reset transform to Vector3(-422.34, 18.23, 0)
         if(transform.position.x >= finishline.transform.position.x) {
-            Debug.Log(beat);
             transform.position = startLine.transform.position;
         }
 
