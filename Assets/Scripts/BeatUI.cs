3675d2b86be722b92113f8926196e7a314cb3cea
Satvik Srinivasan <ssrini17@ucsc.edu>
2021-03-17 18:34:31 -0700

    Update BeatUI.cs

--- Assets/Scripts/BeatUI.cs
+++ Assets/Scripts/BeatUI.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BeatUI : MonoBehaviour
 {
     public GameObject ShootingTiming;
-    private GameObject finishline = GameObject.Find("lineStop");
+    private GameObject finishline;
     private Vector2 direction = new Vector2(3.61f, 0);
     private float tempo;
     private int beat;
@@ -15,6 +15,7 @@ public class BeatUI : MonoBehaviour
         ShootingTiming = GameManager.Instance.GetShootingTiming();
         tempo = GameManager.Instance.getTempo();
         beat = GameManager.Instance.GetShootingTiming().GetComponent<ShootingTiming>().getBeat();
+        finishline = GameObject.Find("lineStop");
 
     }
 
@@ -23,13 +24,14 @@ public class BeatUI : MonoBehaviour
     {
         // get the tempo timing per millisecond
         // get transform of LineStart
+        beat = GameManager.Instance.GetShootingTiming().GetComponent<ShootingTiming>().getBeat();
         transform.Translate(direction / tempo * Time.deltaTime);
     }
     void FixedUpdate() {
         // get beat
         // if = 0, reset transform to Vector3(-422.34, 18.23, 0)
-        if(Vector3.Distance(finishline.GetComponent<Transform>.position, transform.position) == 0) {
-            transform = new Vector3(-422.34f, 18.23f, 0f);
+        if(beat == 0) {
+            transform.position = finishline.position;
         }
 
     }
